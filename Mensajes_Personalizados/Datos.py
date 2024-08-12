f47f3620accb779c56ea5d35600ea61a5b44fc7a
Skrexz <xsebastianrojasx@gmail.com>
2024-08-12 10:53:32 -0500

    Datos

---
+++ Mensajes_Personalizados/Datos.py
@@ -0,0 +1,7 @@
+nombre = input("Ingrese su nombre ")
+edad = input("Ingrese su edad ")
+documento = input("Ingrese su documento ")
+correo = input("Ingrese su correo ")
+carrera = input("Ingrese su carrera ")
+
+print(f"Su nombre es {nombre}, tiene {edad} años, su documento es {documento}, su correo electronico es {correo} y su carrera es {carrera}")
